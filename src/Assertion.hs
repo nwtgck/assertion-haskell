d30195dca1f34ef545c9e5313622580afd28ef14
Ryo Ota <nwtgck@gmail.com>
2018-04-15 12:57:01 +0900

    [add] Implement `assertM` to fail show condition expression

--- src/Assertion.hs
+++ src/Assertion.hs
@@ -1,6 +1,25 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Assertion where
+
+import qualified Language.Haskell.TH as TH
+import qualified Language.Haskell.TH.Quote as TH.Quote
+import qualified Language.Haskell.Meta as Haskell.Meta
 
 -- | assert
 myAssert :: Bool -> a -> a
 myAssert False _ = error "assertion failed"
 myAssert _     x = x
+
+-- | Assert in monad
+assertM :: TH.Quote.QuasiQuoter
+assertM = TH.Quote.QuasiQuoter
+          { TH.Quote.quoteExp = \condStr -> do
+              let Right condExp = Haskell.Meta.parseExp condStr
+              [|if $(return condExp)
+                  then return ()
+                  else error ("Assertion failed: '" ++ condStr ++ "'")|]
+          , TH.Quote.quotePat  = undefined
+          , TH.Quote.quoteType = undefined
+          , TH.Quote.quoteDec  = undefined
+          }
--- test/AssertionSpec.hs
+++ test/AssertionSpec.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module AssertionSpec where
 
 import Test.Hspec
@@ -14,8 +17,21 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "myAssert" $ do
-    it "should be nothing" $ do
+    it "should pass" $ do
       myAssert True 'a' `shouldBe` 'a'
     
     it "should be failed" $ do
       evaluate (myAssert False 'a') `shouldThrow` errorCall "assertion failed"
+
+  describe "assertM" $ do
+    it "should pass" $ do
+      let a = "hello"
+      let b = 10
+      actual <- [assertM|a == "hello" && b > 3|]
+      actual `shouldBe` ()
+
+    it "should be failed" $ do
+      let a = "hello"
+      let b = 10
+      let actual = [assertM|a == "hello, world" && b > 99999|] :: IO ()
+      evaluate actual `shouldThrow` errorCall "Assertion failed: 'a == \"hello, world\" && b > 99999'"
